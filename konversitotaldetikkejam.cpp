43a0b418f021d09961495ae5b6d5a367b225ebf2
MuadzMutakhir10 <74224762+MuadzMutakhir10@users.noreply.github.com>
2020-12-03 17:28:32 +0700

    tuga 5

---
+++ konversitotaldetikkejam.cpp
@@ -0,0 +1,15 @@
+//konversi total detik ke Jam, Menit dan Detik
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int td,j,m,d,sm;
+	cout<<"Masukan total detik : ";
+	cin>>td;
+	j=td/3600;
+	sm=td%3600;
+	m=sm/60;
+	d=sm%60;
+	cout<<j<<':'<<m<<":"<<d;
+	return 0;
+}
